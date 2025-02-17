8bb8128ddf63ec75cea83a246b163874237e7d22
Allyedge <alimarslank@gmail.com>
2025-02-17 16:28:45 +0100

    Update project structure

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ export PYTHONPATH=./packages:$PYTHONPATH
 
 pip install --no-index --find-links=./dependencies/ ./dependencies/socha-1.0.8-py3-none-any.whl ./dependencies/xsdata-22.7-py3-none-any.whl --target=./packages/ --cache-dir=./.pip_cache
 
-python3 ./main.py "$@"
+python3 ./bot/main.py "$@"
