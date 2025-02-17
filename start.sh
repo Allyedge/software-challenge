ec6944bb1eb38f9d9b83330fb09d97f000750eeb
Allyedge <alimarslank@gmail.com>
2025-02-17 16:12:03 +0100

    Update start script

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ export XDG_CACHE_HOME=./.pip_cache
 
 export PYTHONPATH=./packages:$PYTHONPATH
 
-# not finished
-# pip install --no-index --find-links=./dependencies/ ./dependencies/socha-1.0.1-py3-none-any.whl ./my_player/dependencies/xsdata-22.7-py3-none-any.whl --target=./packages/ --cache-dir=./.pip_cache
+pip install --no-index --find-links=./dependencies/ ./dependencies/socha-1.0.8-py3-none-any.whl ./dependencies/xsdata-22.7-py3-none-any.whl --target=./packages/ --cache-dir=./.pip_cache
 
 python3 ./src/software_challenge/__init__.py "$@"
